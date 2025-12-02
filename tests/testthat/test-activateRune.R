198b15108ebcf70c95d7b41f5ac308eaee10751c
Jduraisingh <josephduraisingh@gmail.com>
2025-12-02 11:33:07 -0500

    Cleaning package

--- tests/testthat/test-activateRune.R
+++
@@ -1,7 +0,0 @@
-test_that("activateRune harmonizes and adds layers", {
-  runes <- list(sample1 = demo_rune_data())
-  harmonized <- activateRune(runes)
-
-  expect_true("harmonized" %in% names(harmonized[[1]]@assays$RNA@layers))
-  expect_true(all(harmonized[[1]]@misc$harmonized))
-})
